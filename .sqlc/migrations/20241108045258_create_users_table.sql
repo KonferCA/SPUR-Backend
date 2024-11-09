ec5990ea50399b2d1448c4c1066a283bcb15cf0a
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-08 20:06:03 -0500

    Add StatementBegin and StatementEnd

--- .sqlc/migrations/20241108045258_create_users_table.sql
+++ .sqlc/migrations/20241108045258_create_users_table.sql
@@ -1,4 +1,5 @@
 -- +goose Up
+-- +goose StatementBegin
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
@@ -12,6 +13,9 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
+-- +goose StatementEnd
 
 -- +goose Down
+-- +goose StatementBegin
 DROP TABLE users;
+-- +goose StatementEnd
