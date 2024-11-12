4b22d5f124bfd72b6b4cd237d2aa22241b1ca5fa
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-11 22:58:35 -0400

    add create and update request for employees

--- internal/server/types.go
+++ internal/server/types.go
@@ -101,3 +101,17 @@ type CreateCompanyFinancialsRequest struct {
 	ARR            float64 `json:"arr" validate:"required"`
 	GrantsReceived float64 `json:"grants_received" validate:"required"`
 }
+
+type CreateEmployeeRequest struct {
+	CompanyID string `json:"company_id" validate:"required,uuid"`
+	Name      string `json:"name" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Role      string `json:"role" validate:"required"`
+	Bio       string `json:"bio"`
+}
+
+type UpdateEmployeeRequest struct {
+	Name string `json:"name" validate:"required"`
+	Role string `json:"role" validate:"required"`
+	Bio  string `json:"bio"`
+}
